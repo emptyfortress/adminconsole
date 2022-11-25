dac3469bf5912612a4a3a9583864eb6f9422ddcc
Volkov Alexander <volkov.525@gmail.com>
2022-11-25 13:26:14 +0300

    change icons behaviors

--- src/components/SetupRoot.vue
+++ src/components/SetupRoot.vue
@@ -1,74 +1,115 @@
 <template lang="pug">
 div
-	.zag Настройки
 	.inf
 		span Адрес хранилища настроек:
 		a(href="#").q-ml-md http://sol2016.digdes.com:5200/api
+	.zag Настройки
+	q-input(v-model="search" dense clearable autofocus ).search
+		template(v-slot:prepend)
+			q-icon(name="mdi-magnify")
 	br
 	.mygrid
 		router-link(to="/setup/dvserver").bl
+			.icon.serverdv
 			.tit Сервер Docsvision
-			q-img(src="@/assets/img/serverdv.svg" width="26px")
 		router-link(to="/setup/connection").bl
+			.icon.connection
 			.tit Соединения
-			q-img(src="@/assets/img/connection.svg" width="42px")
 		router-link(to="/setup/email").bl
+			.icon.mail
 			.tit Почтовый сервер
-			q-img(src="@/assets/img/mailserver.svg" width="43px")
 		router-link(to="/setup/worker").bl
+			.icon.worker
 			.tit Служба фоновых операций
-			q-img(src="@/assets/img/worker.svg" width="35px")
 		.bl.disable
+			.icon.webclient
 			.tit Web-клиент
-			q-img(src="@/assets/img/webclient.svg" width="38px")
 		.bl.disable
+			.icon.workflow
 			.tit Сервис управления бизнес-процессам
-			q-img(src="@/assets/img/workflow.svg" width="32px")
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const search = ref()
+</script>
 
 <style scoped lang="scss">
+.search {
+	width: 350px;
+	margin: 0 auto;
+	font-size: 1.1rem;
+	margin-bottom: 1rem;
+}
 .mygrid {
 	display: grid;
 	grid-template-columns: repeat(3, 220px);
-	gap: 1rem;
+	// gap: 1rem;
 	margin: 0 auto;
 	// background: pink;
 	max-width: 692px;
 	.bl {
 		width: 220px;
-		height: 180px;
-		background: var(--bg-drawer);
+		// height: 180px;
+		// background: var(--bg-drawer);
 		font-size: 1.2rem;
 		color: $secondary;
-		border-radius: 0.5rem;
+		// border-radius: 0.5rem;
 		padding: 1rem;
-		border: 1px dotted $secondary;
+		border: 1px solid transparent;
 		position: relative;
 		cursor: pointer;
 		transition: 0.2s ease all;
 		text-decoration: none;
-		.q-img {
-			position: absolute;
-			bottom: 1rem;
-			left: 1rem;
+		.icon {
+			width: 36px;
+			height: 36px;
+			margin-bottom: 0.5rem;
+			&.mail {
+				background: url('@/assets/img/mailserver.svg');
+				width: 47px;
+			}
+			&.serverdv {
+				background: url('@/assets/img/serverdv.svg');
+			}
+			&.worker {
+				background: url('@/assets/img/worker.svg');
+			}
+			&.connection {
+				background: url('@/assets/img/connection.svg');
+			}
+			&.webclient {
+				background: url('@/assets/img/webclient.svg');
+			}
+			&.workflow {
+				background: url('@/assets/img/workflow.svg');
+			}
 		}
+
 		&:hover {
-			box-shadow: 4px 4px 16px rgba(0, 0, 0, 0.3);
-			border: 1px solid #fff;
-			color: #000;
+			border: 1px solid $primary;
+			.webclient,
+			.mail,
+			.serverdv,
+			.worker,
+			.connection,
+			.workflow {
+				background-position: bottom;
+			}
 		}
 		&.disable {
 			opacity: 0.5;
 			cursor: default;
 			box-shadow: none;
-			border: 1px dotted $secondary;
+			border: none;
 		}
 	}
 }
 .inf {
-	text-align: center;
+	text-align: right;
+	font-size: 0.8rem;
+	color: grey;
 }
 @media screen and (max-width: 750px) {
 	.mygrid {
