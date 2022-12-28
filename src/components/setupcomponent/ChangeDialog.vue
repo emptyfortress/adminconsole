2dbc277af8223962005b49d96f14c5913b48d261
Volkov Alexander <volkov.525@gmail.com>
2022-12-28 13:28:26 +0300

    clea dialog

---
+++ src/components/setupcomponent/ChangeDialog.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+q-dialog(v-model="change")
+	q-card.q-pa-sm
+		q-card-section(class="row items-center q-pb-none")
+			div(class="text-h6") База данных по умолчанию
+			q-space
+			q-btn(icon="close" flat round dense v-close-popup)
+		q-card-section Вы действительно хотите назначить <span class="name">{{changename}}</span> в качестве базы по умолчанию? Это может занять некоторoе время, но измения вступят в силу сразу.
+		q-card-section.def При обращении к серверу Docsvision без ввода дополнительных параметров, происходит подключение к базе данных по умолчанию. Эта же база обрабатывается сервисами.
+		q-card-actions(align="right")
+			q-btn(flat label="Отмена" color="primary" v-close-popup)
+			q-btn(flat label="Назначить" color="primary" :loading="loading" @click="assignDefault")
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/setupcomponent/MasterBD.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+q-dialog(:model-value="dialog" position="bottom" @update:model-value="$emit('toggle')")
+	q-card(style="width: 350px")
+		p lakjsdljal
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const props = defineProps({
+	dialog: {
+		type: Boolean,
+		default: false,
+	},
+})
+</script>
+
+<style scoped lang="scss"></style>
