34bac72922ebb1eb7e41ce48722219157864dadc
Volkov Alexander <volkov.525@gmail.com>
2023-01-16 13:31:07 +0300

    dserver1 pug -> html done

--- src/components/Dvserver1.vue
+++ src/components/Dvserver1.vue
@@ -1,22 +1,29 @@
-<template lang="pug">
-div
-	component(:is="AddConnection" v-model="dialog" @add="addConfig" dv)
-	.zag Настройки сервера Docsvision 1
-	.bar
-		q-chip(color="warning") Всего конфигураций: {{store.config.length}}
-		q-btn(flat round dense icon="mdi-plus-circle" @click="add").plus
-			q-tooltip Добавить конфигурцию
-		q-space
-		.alltab
-			q-tabs(v-model="store.tabs" active-color="primary" inline-label ).text-secondary
-				q-tab(v-for="item in store.config" :key="item.id" :name="item.name" :label="item.name")
-
-	q-card
-		q-tab-panels(v-model="store.tabs" animated)
-			template(v-for="item in store.config" :key="item.id")
-				q-tab-panel(:name="item.name")
-					component(:is="GreyBlock4" :name="item.name" @delete="store.removeConfig(item)" @duble="addConfig(item.name, true)")
-
+<template>
+	<div>
+		<AddConnection v-model="dialog" @add="addConfig" dv />
+		<div class="zag">Настройки сервера Docsvision 1</div>
+		<div class="bar">
+			<q-chip color="warning">Всего конфигураций: {{ store.config.length }}</q-chip>
+			<q-btn class="plus" flat round dense icon="mdi-plus-circle" @click="add">
+				<q-tooltip>Добавить конфигурцию</q-tooltip>
+			</q-btn>
+			<q-space></q-space>
+			<div class="alltab">
+				<q-tabs class="text-secondary" v-model="store.tabs" active-color="primary" inline-label>
+					<q-tab v-for="item in store.config" :key="item.id" :name="item.name" :label="item.name"></q-tab>
+				</q-tabs>
+			</div>
+		</div>
+		<q-card>
+			<q-tab-panels v-model="store.tabs" animated>
+				<template v-for="item in store.config" :key="item.id">
+					<q-tab-panel :name="item.name">
+						<GreyBlock4 :name="item.name" @delete="store.removeConfig(item)" @duble="addConfig(item.name, true)" />
+					</q-tab-panel>
+				</template>
+			</q-tab-panels>
+		</q-card>
+	</div>
 
 </template>
 
@@ -54,20 +61,24 @@ const addConfig = (e: string, copy: boolean) => {
 	display: flex;
 	align-items: center;
 	padding: 0 0.5rem;
+
 	@media screen and (max-width: 1233px) {
 		display: block;
 	}
 }
+
 .q-card {
 	max-width: clamp(600px, 70%, 1200px);
 	margin: 0 auto;
 	background: var(--bg-light);
 }
+
 .plus {
 	:deep(.q-icon) {
 		color: $primary;
 	}
 }
+
 .alltab {
 	max-width: 700px;
 }
