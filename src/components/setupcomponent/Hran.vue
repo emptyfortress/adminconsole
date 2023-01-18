e0d34fbf554373176a9e0d257085f8c18b0ee017
Volkov Alexander <volkov.525@gmail.com>
2023-01-18 13:24:59 +0300

    continue

--- src/components/setupcomponent/Hran.vue
+++ src/components/setupcomponent/Hran.vue
@@ -7,25 +7,44 @@ const list1 = ref([
 	{ name: 'Storage 2', id: 2 },
 ])
 
+const remove1 = (n: number) => {
+	list1.value.splice(n, 1)
+}
 </script>
 
 <template lang="pug">
 .row.items-center.justify-between
 	.zg Хранилища ({{ list1.length }})
 	q-btn(flat round icon="mdi-plus-circle")
-.grey
-	draggable.list-group(:list="list1" item-key="id" :group="{ name: 'group', pull: 'clone', put: false }" ghost-class='ghost' :move='checkMove' @start='dragging = true' @end='dragging = false')
+.grey.hran
+	draggable(:list="list1"
+		item-key="id"
+		:group="{ name: 'group', pull: 'clone', put: false }"
+		ghost-class='ghost'
+		:move='checkMove'
+		@start='dragging = true'
+		@end='dragging = false'
+		).list-group
 		template(#item="{ element, index }")
 			.row.justify-between.items-center
 				div
 					q-icon(name="mdi-database-outline" size="18px" style="vertical-align: top;")
 					span.q-ml-sm {{ element.name }}
-				q-btn(flat round dense icon="mdi-trash-can-outline" size="10px" @click="remove1(index)")
+				q-btn(flat round dense icon="mdi-trash-can-outline" size="10px" )
+					q-menu
+						q-list
+							q-item(clickable v-close-popup @click="remove1(index)").pink
+								q-item-section Подтверждаю
 
 </template>
 
 <style scoped lang="scss">
-.grey {
+.zg {
+	font-size: 1.1rem;
+	color: $secondary;
+}
+
+.hran {
 	margin-top: 0;
 	margin-bottom: 1rem;
 	position: relative;
