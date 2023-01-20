0f6661f7a6b4d0d23ca9ec8b916a948c163eb92c
Volkov Alexander <volkov.525@gmail.com>
2023-01-20 16:12:16 +0300

    add splitter

--- src/components/setupcomponent/Outer.vue
+++ src/components/setupcomponent/Outer.vue
@@ -1,32 +1,29 @@
 <template lang="pug">
 .outer
-	div
-		Hran
+	q-splitter(v-model="split").spl
+		template(#before)
+			Hran
+			Rule
 
-	.group
-		Group
-
-	div
-		Rule
+		template(#after)
+			Group
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Group from '@/components/setupcomponent/Group.vue'
 import Hran from '@/components/setupcomponent/Hran.vue'
 import Rule from '@/components/setupcomponent/Rule.vue'
+
+const split = ref(50)
 </script>
 
 <style scoped lang="scss">
-.outer {
-	display: grid;
-	grid-template-columns: 4fr 3fr;
-	gap: 1rem;
-	padding-top: 1rem;
+.spl {
+	height: calc(100vh - 360px);
 }
 
-.group {
-	grid-column: 2/3;
-	grid-row: 1/3;
-
+.outer {
+	padding-top: 1rem;
 }
 </style>
