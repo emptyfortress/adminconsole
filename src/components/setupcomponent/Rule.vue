02f277c4f5a6c5fd9ca03e96c7e36a7f577a5127
Volkov Alexander <volkov.525@gmail.com>
2023-01-20 11:34:34 +0300

    fix types

--- src/components/setupcomponent/Rule.vue
+++ src/components/setupcomponent/Rule.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, } from 'vue'
+import type { Ref } from 'vue'
 import draggable from 'vuedraggable'
+
 
 const showAdd = ref(false)
 
@@ -12,11 +14,20 @@ const ext = ref()
 const size = ref()
 const size1 = ref()
 
-const list = ref([])
-
+const list: Ref<Rule[]> = ref([])
 const date = new Date()
+
+interface Rule {
+	id: number
+	name: string
+	type: string
+	ext: string
+	size: number
+	size1: number
+}
+
 const addRule = (() => {
-	let tmp = {}
+	let tmp = {} as Rule
 	tmp.id = +date
 	tmp.name = newRuleName.value
 	tmp.type = type.value
@@ -25,13 +36,21 @@ const addRule = (() => {
 	tmp.size1 = size1.value
 	list.value.push(tmp)
 	newRuleName.value = null
-	type.value = null
+	type.value = 'Все'
 	ext.value = null
 	size.value = null
 	size1.value = null
 	showAdd.value = false
 })
 
+const showDialog = (() => {
+	newRuleName.value = null
+	type.value = 'Все'
+	ext.value = null
+	size.value = null
+	size1.value = null
+	showAdd.value = true
+})
 const remove = ((ind: number) => {
 	list.value.splice(ind, 1)
 })
@@ -40,7 +59,7 @@ const remove = ((ind: number) => {
 <template lang="pug">
 .row.items-center.justify-between
 	.zg Правила помещения в хранилище ({{ list.length }})
-	q-btn(flat round icon='mdi-plus-circle' @click="showAdd = true")
+	q-btn(flat round icon='mdi-plus-circle' @click="showDialog")
 .grey
 	div(v-if="list.length === 0") Создайте первое правило
 	component(:is="draggable" :list="list"
@@ -56,19 +75,13 @@ const remove = ((ind: number) => {
 					span.q-ml-sm {{ element.name }}
 				.desc {{ element.type }}
 				.desc(v-if="element.type === 'Все'") &nbsp;
+				.desc(v-if="element.type === 'Файл справочника'") &nbsp;
+				.desc(v-if="element.type === 'Добавить из сборки'") &nbsp;
 				.desc(v-if="element.ext") {{ element.ext }}
 				.desc(v-if="element.size") {{ element.size }} Gb
 				.desc(v-if="element.size1") {{ element.size1 }} Gb
 
-				div
-					q-icon(name="mdi-information" size="sm").q-mr-md
-						q-menu
-							q-card(flat)
-								.hrinfo
-									template(v-for="prop in element.props")
-										.label {{ prop[0]}}:
-										.val {{ prop[1]}}
-
+				.right
 					q-btn(flat round dense icon="mdi-pencil" size="sm" @click="edit(index)" )
 					q-btn(flat round dense icon="mdi-trash-can-outline" size="sm" )
 						q-menu
@@ -79,23 +92,24 @@ const remove = ((ind: number) => {
 
 q-dialog(v-model="showAdd")
 	q-card(style="min-width: 500px;")
-		q-card-section.row.items-center.q-pb-none
-			.text-h6 Новое правило
-			q-space
-			q-btn(icon="mdi-close" flat round dense v-close-popup)
+		q-form(@submit="addRule")
+			q-card-section.row.items-center.q-pb-none
+				.text-h6 Новое правило
+				q-space
+				q-btn(icon="mdi-close" flat round dense v-close-popup)
 
-		q-card-section
-			q-input(v-model='newRuleName' autofocus label='Название')
-			.grid
-				q-select(v-model='type' label='Тип' :options="options").full-width
-				q-input(v-model="ext" label="Расширение" v-if="type === 'По расширению файла'").full-width
-				q-input(v-model="size" label="Размер" v-if="type === 'Размер больше, чем'" type="number").full-width
-				q-input(v-model="size1" label="Размер" v-if="type === 'Размер меньше, чем'" type="number").full-width
+			q-card-section
+				q-input(v-model='newRuleName' autofocus label='Название' lazy-rules :rules="[val => val && val.length > 0 || 'Обязательное поле']")
+				.grid
+					q-select(v-model='type' label='Тип' :options="options").full-width
+					q-input(v-model="ext" label="Расширение" v-if="type === 'По расширению файла'" lazy-rules :rules="[val => val && val.length > 0 || 'Обязательное поле']").full-width
+					q-input(v-model="size" label="Размер" v-if="type === 'Размер больше, чем'" type="number" lazy-rules :rules="[val => val !== null && val !== '' || 'Обязательное поле']").full-width
+					q-input(v-model="size1" label="Размер" v-if="type === 'Размер меньше, чем'" type="number").full-width
 
-		q-card-section
-			q-card-actions(align="right")
-				q-btn(flat color='primary' label='Отмена' @click='showAdd = false')
-				q-btn(unelevated color='primary' label='Добавить' @click='addRule')
+			q-card-section
+				q-card-actions(align="right")
+					q-btn(flat color='primary' label='Отмена' @click='showAdd = false')
+					q-btn(unelevated color='primary' label='Добавить' type="submit")
 </template>
 
 <style scoped lang="scss">
@@ -129,7 +143,7 @@ q-dialog(v-model="showAdd")
 .tabel {
 	width: 100%;
 	display: grid;
-	grid-template-columns: 1fr 150px 70px 90px;
+	grid-template-columns: 1fr 150px 70px 100px;
 	justify-items: start;
 	column-gap: 1rem;
 	row-gap: .2rem;
@@ -137,5 +151,9 @@ q-dialog(v-model="showAdd")
 
 .desc {
 	font-size: 0.8rem;
+}
+
+.right {
+	justify-self: end;
 }
 </style>
