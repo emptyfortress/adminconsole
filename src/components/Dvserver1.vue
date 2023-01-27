d2dd9053d23c7eb74ea93a7a3777a4820c299474
Volkov Alexander <volkov.525@gmail.com>
2023-01-27 12:44:35 +0300

    make width of panels larger

--- src/components/Dvserver1.vue
+++ src/components/Dvserver1.vue
@@ -1,30 +1,60 @@
 <template>
 	<div>
-		<AddConnection v-model="dialog" @add="addConfig" dv />
+		<AddConnection
+			v-model="dialog"
+			@add="addConfig"
+			dv
+		/>
 		<div class="zag">Настройки сервера Docsvision 1</div>
 		<div class="bar">
 			<q-chip color="warning">Всего конфигураций: {{ store.config.length }}</q-chip>
-			<q-btn class="plus" flat round dense icon="mdi-plus-circle" @click="add">
+			<q-btn
+				class="plus"
+				flat
+				round
+				dense
+				icon="mdi-plus-circle"
+				@click="add"
+			>
 				<q-tooltip>Добавить конфигурцию</q-tooltip>
 			</q-btn>
 			<q-space></q-space>
 			<div class="alltab">
-				<q-tabs class="text-secondary" v-model="store.tabs" active-color="primary" inline-label>
-					<q-tab v-for="item in store.config" :key="item.id" :name="item.name" :label="item.name"></q-tab>
+				<q-tabs
+					class="text-secondary"
+					v-model="store.tabs"
+					active-color="primary"
+					inline-label
+				>
+					<q-tab
+						v-for="item in store.config"
+						:key="item.id"
+						:name="item.name"
+						:label="item.name"
+					></q-tab>
 				</q-tabs>
 			</div>
 		</div>
 		<q-card>
-			<q-tab-panels v-model="store.tabs" animated>
-				<template v-for="item in store.config" :key="item.id">
+			<q-tab-panels
+				v-model="store.tabs"
+				animated
+			>
+				<template
+					v-for="item in store.config"
+					:key="item.id"
+				>
 					<q-tab-panel :name="item.name">
-						<GreyBlock4 :name="item.name" @delete="store.removeConfig(item)" @duble="addConfig(item.name, true)" />
+						<GreyBlock4
+							:name="item.name"
+							@delete="store.removeConfig(item)"
+							@duble="addConfig(item.name, true)"
+						/>
 					</q-tab-panel>
 				</template>
 			</q-tab-panels>
 		</q-card>
 	</div>
-
 </template>
 
 <script setup lang="ts">
@@ -56,7 +86,7 @@ const addConfig = (e: string, copy: boolean) => {
 <style scoped lang="scss">
 .bar {
 	background: #dedede;
-	max-width: clamp(600px, 70%, 1200px);
+	max-width: clamp(600px, 80%, 1200px);
 	margin: 0 auto;
 	display: flex;
 	align-items: center;
@@ -68,7 +98,7 @@ const addConfig = (e: string, copy: boolean) => {
 }
 
 .q-card {
-	max-width: clamp(600px, 70%, 1200px);
+	max-width: clamp(600px, 80%, 1200px);
 	margin: 0 auto;
 	background: var(--bg-light);
 }
