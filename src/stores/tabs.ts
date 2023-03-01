8b0e8fab18014cc29baed67ebecdb612c9942b40
Volkov Alexander <volkov.525@gmail.com>
2023-03-01 19:25:06 +0300

    fix type errors

--- src/stores/tabs.ts
+++ src/stores/tabs.ts
@@ -4,12 +4,12 @@ export const useTabs = defineStore({
 	id: 'tabs',
 	state: () => ({
 		tabs: [
-			{ id: 0, name: 'prop', label: 'Свойства', modified: false },
-			{ id: 1, name: 'control', label: 'Управление', modified: false },
-			{ id: 2, name: 'outer', label: 'Внешние хранилища', modified: false },
-			{ id: 3, name: 'arch', label: 'Архивирование', modified: false },
-			{ id: 4, name: 'meta', label: 'Метаданные', modified: false },
-			{ id: 5, name: 'cache', label: 'Кэширование', modified: false },
+			{ id: 0, field: 'prop', name: 'prop', label: 'Свойства', modified: false },
+			{ id: 1, field: 'control', name: 'control', label: 'Управление', modified: false },
+			{ id: 2, field: 'outer', name: 'outer', label: 'Внешние хранилища', modified: false },
+			{ id: 3, field: 'arch', name: 'arch', label: 'Архивирование', modified: false },
+			{ id: 4, field: 'meta', name: 'meta', label: 'Метаданные', modified: false },
+			{ id: 5, field: 'cache', name: 'cache', label: 'Кэширование', modified: false },
 		],
 	}),
 	getters: {},
