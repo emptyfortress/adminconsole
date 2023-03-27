10ba359cf164df8e19ec15924dc5c907d996973a
Volkov Alexander <volkov.525@gmail.com>
2023-03-27 14:02:06 +0300

    add drag table on bd

--- src/components/setupcomponent/MasterBD.vue
+++ src/components/setupcomponent/MasterBD.vue
@@ -1,30 +1,42 @@
 <template lang="pug">
 q-dialog(:model-value="props.dialog" position="bottom" full-width persistent)
 	q-card.edit
-		q-btn.close(flat round icon="mdi-close" color="primary" @click="close")
-
 		div
 			q-card-section
 				.title
 					q-icon(name='mdi-database-cog')
 					span Мастер баз данных
 
-			q-tabs.text-secondary(v-model='mytab' dense)
-				q-tab(v-for='tab in tabs' :key='tab.name' :name='tab.name') {{tab.label}}
+			.all
+				.arch
+					component(:is="draggable" :list="list"
+						item-key="id"
+						ghost-class='ghost'
+						).list-group
 
-			// q-stepper(v-model="step" ref="stepper" color="primary" animated flat)
-			// 	q-step(:name="1" title="Select campaign settings" icon="settings" :done="step > 1") laksjl
-    
+						template(#header)
+							.tabel.gr
+								div Название БД
+								div Доступно пользователям ДВ
+
+						template(#item="{ element, index }")
+							.tabel(:class="{ selected : element.selected }" @click="select(index)")
+								div
+									q-icon(name="mdi-database-outline" size="18px" style="vertical-align: top;")
+									span.q-ml-sm {{ element.name }}
+								q-checkbox(v-model="act" :val="element.id" dense)
 
 		div
 			q-separator
 			q-card-actions(align='center')
 				q-btn(flat color="primary" @click="close") Отмена
 				q-btn(unelevated color="primary" @click="close") Применить
+		q-btn.close(flat round icon="mdi-close" color="primary" @click="close")
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import draggable from 'vuedraggable'
 
 const props = defineProps({
 	dialog: {
@@ -38,20 +50,34 @@ const close = () => {
 	emit('update:modelValue', false)
 }
 
-const mytab = ref('create')
-const tabs = [
-	{ id: 0, field: 'create', name: 'create', label: 'Создание БД', modified: false },
-	{ id: 1, field: 'update', name: 'update', label: 'Обновление БД', modified: false },
+const act = ref([0, 1])
+
+const list = reactive([
+	{
+		id: 0,
+		name: 'DV_management',
+		selected: false,
+	},
+	{
+		id: 1,
+		name: 'lyahova_dv6',
+		selected: false,
+	},
 	{
 		id: 2,
-		field: 'connect',
-		name: 'connect',
-		label: 'Подключение к существующей БД',
-		modified: false,
+		name: 'lyahova_gbl_2926',
+		selected: false,
 	},
-]
-
-// const step = ref(1)
+	{
+		id: 3,
+		name: 'nefedov_gbl_3132',
+		selected: false,
+	},
+])
+const select = (ind: number) => {
+	list.map((item) => (item.selected = false))
+	list[ind].selected = true
+}
 </script>
 
 <style scoped lang="scss">
@@ -78,5 +104,32 @@ const tabs = [
 	position: absolute;
 	top: 1rem;
 	right: 1rem;
+}
+.all {
+	width: 900px;
+	margin: 0 auto;
+}
+.arch {
+	background: var(--bg-grey);
+	padding: 1rem;
+}
+.titl {
+	font-size: 0.9rem;
+	text-transform: uppercase;
+	color: $secondary;
+}
+.tabel {
+	width: 100%;
+	display: grid;
+	grid-template-columns: 1fr 230px;
+	justify-items: start;
+	align-items: center;
+	grid-column: 1/-1;
+	&.gr {
+		color: #666;
+	}
+	&.selected {
+		background: var(--bg-selected);
+	}
 }
 </style>
